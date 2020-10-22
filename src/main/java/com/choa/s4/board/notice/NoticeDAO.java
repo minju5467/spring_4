8f5dfedf92ba0b8ebd4d1e43e1d4956b5850ec3e
Daeki <kimdaeki@gmail.com>
2020-10-22 11:05:27 +0900

    add_list

--- src/main/java/com/choa/s4/board/notice/NoticeDAO.java
+++ src/main/java/com/choa/s4/board/notice/NoticeDAO.java
@@ -2,16 +2,24 @@ package com.choa.s4.board.notice;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.choa.s4.board.BoardDAO;
 import com.choa.s4.board.BoardDTO;
 import com.choa.s4.util.Pager;
-
+@Repository
 public class NoticeDAO implements BoardDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.choa.s4.board.notice.NoticeDAO.";
 
 	@Override
 	public int setInsert(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.insert(NAMESPACE+"setInsert", boardDTO);
 	}
 
 	@Override
@@ -29,7 +37,7 @@ public class NoticeDAO implements BoardDAO {
 	@Override
 	public List<BoardDTO> getList(Pager pager) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		return sqlSession.selectList(NAMESPACE+"getList", pager);
 	}
 
 	@Override
--- src/main/java/com/choa/s4/board/qna/QnaDAO.java
+++ src/main/java/com/choa/s4/board/qna/QnaDAO.java
@@ -2,16 +2,26 @@ package com.choa.s4.board.qna;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.choa.s4.board.BoardDAO;
 import com.choa.s4.board.BoardDTO;
 import com.choa.s4.util.Pager;
 
+@Repository
 public class QnaDAO implements BoardDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.choa.s4.board.qna.QnaDAO.";
+	
 
 	@Override
 	public int setInsert(BoardDTO boardDTO) throws Exception {
 		// TODO Auto-generated method stub
-		return 0;
+		return sqlSession.insert(NAMESPACE+"setInsert", boardDTO);
 	}
 
 	@Override
