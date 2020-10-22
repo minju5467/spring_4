732477da930081f3f847fa0e548707973082a407
Daeki <kimdaeki@gmail.com>
2020-10-22 10:01:00 +0900

    add_board

---
+++ src/main/java/com/choa/s4/board/notice/NoticeDAO.java
@@ -0,0 +1,49 @@
+package com.choa.s4.board.notice;
+
+import java.util.List;
+
+import com.choa.s4.board.BoardDAO;
+import com.choa.s4.board.BoardDTO;
+import com.choa.s4.util.Pager;
+
+public class NoticeDAO implements BoardDAO {
+
+	@Override
+	public int setInsert(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setUpdate(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int setDelete(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public List<BoardDTO> getList(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public BoardDTO getOne(BoardDTO boardDTO) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getCount(Pager pager) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+	
+
+}
