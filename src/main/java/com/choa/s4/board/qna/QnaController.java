a1334bf20764dd8ec0fba9ecab3fca1af9b1388e
Daeki <kimdaeki@gmail.com>
2020-11-04 11:15:12 +0900

    ddd

--- src/main/java/com/choa/s4/board/qna/QnaController.java
+++ src/main/java/com/choa/s4/board/qna/QnaController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -150,14 +151,16 @@ public class QnaController {
 		ModelAndView mv = new ModelAndView();
 		List<BoardDTO> ar = qnaService.getList(pager);
 		
+		ar = null;
 		BoardDTO boardDTO  = ar.get(0);
 		QnaDTO qnaDTO = (QnaDTO)boardDTO;
 		System.out.println(qnaDTO.getDepth());
+		
 		mv.addObject("board", "qna");
 		mv.addObject("list", ar);
 		mv.addObject("pager", pager);
 		mv.setViewName("board/boardList");
 		return mv;
 	}
-
+	
 }
